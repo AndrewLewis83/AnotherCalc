24a012ac4849e044003d1df9d8525753eb968ed4
Andy Lewis <andrewlewisdev@gmail.com>
2022-07-13 07:04:12 -0600

    Initial Commit

---
+++ AnotherCalc/AnotherCalc.h
@@ -0,0 +1,18 @@
+//
+//  AnotherCalc.h
+//  AnotherCalc
+//
+//  Created by Andy Lewis on 7/13/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnotherCalc.
+FOUNDATION_EXPORT double AnotherCalcVersionNumber;
+
+//! Project version string for AnotherCalc.
+FOUNDATION_EXPORT const unsigned char AnotherCalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnotherCalc/PublicHeader.h>
+
+
